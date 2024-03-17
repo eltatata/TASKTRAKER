1e13a11603c27acd4a9973739f9ce7949458a73d
eltatataa <davidtowo75@gmail.com>
2024-03-16 23:35:42 -0500

    Change task component & sort tasks

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,8 +1,29 @@
+import TaskModel, { ITask } from "@/models/Task";
+import { connectionDB } from "@/lib/database";
+import { auth } from "@clerk/nextjs";
+
 import TaskForm from "@/components/TaskForm";
 import Tasks from "@/components/Tasks";
 import Clock from "@/components/Clock";
 
-export default function TasksPage() {
+const loadTasks = async () => {
+  connectionDB();
+
+  const { userId } = auth();
+
+  let tasks: ITask[] = await TaskModel.find({ uid: userId }).lean();
+
+  tasks = tasks.map(task => {
+    task._id = task._id.toString();
+    return task;
+  });
+
+  return tasks;
+}
+
+export default async function TasksPage() {
+  const tasks = await loadTasks();
+
   return (
     <main className="flex items-start">
       <div className="flex justify-center items-center w-2/5 h-screen">
@@ -11,7 +32,7 @@ export default function TasksPage() {
           <TaskForm />
         </div>
       </div>
-      <Tasks />
+      <Tasks tasks={tasks} />
     </main>
   )
 }
--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,41 +1,73 @@
-import TaskModel, { ITask } from "@/models/Task";
-import { connectionDB } from "@/lib/database";
+"use client";
+
+import { useState } from "react";
+import { ITask } from "@/models/Task";
 import Link from "next/link";
-import { auth } from "@clerk/nextjs";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
-const loadTasks = async () => {
-  connectionDB();
+export default function Tasks({ tasks }: { tasks: ITask[] }) {
+  const [order, setOrder] = useState('date to complete');
 
-  const { userId } = auth();
+  const sortTasks = (orderBy: string) => {
+    switch (orderBy) {
+      case 'date to complete':
+        return tasks.slice().sort((a, b) => new Date(a.dtc).getTime() - new Date(b.dtc).getTime());
+      case 'done':
+        return tasks.slice().sort((a, b) => a.done ? -1 : 1);
+      default:
+        return tasks;
+    }
+  };
 
-  const tasks: ITask[] = await TaskModel.find({ uid: userId }).lean();
-  return tasks;
-}
-
-export default async function Tasks() {
-  const tasks = await loadTasks();
+  const sortedTasks = sortTasks(order);
 
   return (
-    <div className="grid grid-cols-3 gap-10 justify-center w-3/5 p-10">
-      {tasks.map((task, index) => (
-        <Link
-          key={index}
-          className="flex flex-col justify-between gap-3 h-[350px] w-full p-5 rounded-xl bg-black bg-opacity-40 hover:bg-opacity-70 backdrop-blur-xl duration-300 ease-in-out"
-          href={`/tasks/${task._id}`}
-        >
-          <h3 className="text-3xl font-bold">{task.title.toUpperCase()}</h3>
-          <p>{task.description}</p>
-          <p>
-            Date to complete: <b>{new Date(task.dtc).toLocaleDateString()}</b>
-          </p>
-          <p>
-            Created At: <b>{new Date(task.createdAt).toLocaleDateString()}</b>
-          </p>
-          <p className={task.done ? "text-green-500" : "text-red-500"}>
-            <b>{task.done ? "done" : "not done"}</b>
-          </p>
-        </Link>
-      ))}
+    <div className="flex flex-col items-end gap-5 w-3/5 p-10">
+      <div className="flex gap-2">
+        <Select onValueChange={setOrder}>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Order by" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem value="date to complete">Date to complete</SelectItem>
+              <SelectItem value="createdAt">CreatedAt</SelectItem>
+              <SelectItem value="done">Done</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-10">
+        {sortedTasks.map((task) => (
+          <Link
+            key={task._id}
+            className="flex flex-col justify-between gap-3 p-5 rounded-xl bg-black bg-opacity-40 hover:bg-opacity-70 backdrop-blur-xl duration-300 ease-in-out"
+            href={`/tasks/${task._id}`}
+          >
+            <h3 className="text-3xl font-bold">{task.title.toUpperCase()}</h3>
+            <p>{task.description}</p>
+            <p>
+              Date to complete: <b className={new Date(task.dtc) < new Date() && !task.done ? 'text-yellow-500' : ''}>
+                {new Date(task.dtc).toLocaleDateString()}
+                {new Date(task.dtc) < new Date() && !task.done ? <small> expired</small> : null}
+              </b>
+            </p>
+            <p>
+              Created At: <b>{new Date(task.createdAt).toLocaleDateString()}</b>
+            </p>
+            <p className={task.done ? "text-green-500" : "text-red-500"}>
+              <b>{task.done ? "done" : "not done"}</b>
+            </p>
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
