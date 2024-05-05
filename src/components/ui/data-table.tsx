db8d5d2b515951ca615afc222933fa17d8d34176
eltatataa <davidtowo75@gmail.com>
2024-05-05 00:48:07 -0500

    added pagination

--- src/components/ui/data-table.tsx
+++ src/components/ui/data-table.tsx
@@ -21,7 +21,8 @@ import {
   TableRow,
 } from "@/components/ui/table"
 
-import { Button } from "@/components/ui/button"
+import { DataTablePagination } from "@/components/ui/data-table-pagination"
+
 import { Input } from "@/components/ui/input"
 
 interface DataTableProps<TData, TValue> {
@@ -50,6 +51,10 @@ export function DataTable<TData, TValue>({
       columnFilters,
     },
   })
+
+  React.useEffect(() => {
+    table.setPageSize(6)
+  }, [])
 
   return (
     <div>
@@ -107,24 +112,7 @@ export function DataTable<TData, TValue>({
           </TableBody>
         </Table>
       </div>
-      <div className="flex items-center justify-end space-x-2 py-4">
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.previousPage()}
-          disabled={!table.getCanPreviousPage()}
-        >
-          Previous
-        </Button>
-        <Button
-          variant="outline"
-          size="sm"
-          onClick={() => table.nextPage()}
-          disabled={!table.getCanNextPage()}
-        >
-          Next
-        </Button>
-      </div>
+      <DataTablePagination table={table} />
     </div>
   )
 }
