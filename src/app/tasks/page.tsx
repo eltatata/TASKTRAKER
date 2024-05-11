c39900d97f58cbd059c8debab4ef8540aa873f49
eltatataa <davidtowo75@gmail.com>
2024-05-11 01:18:22 -0500

    correction of the task page styles

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -30,12 +30,12 @@ export default async function TasksPage() {
   const tasks = await loadTasks();
 
   return (
-    <main className="flex items-start overflow-x-hidden">
-      <div className="relative flex flex-col items-center w-2/5 gap-10 p-10">
+    <main className="flex items-start gap-5 pt-10 px-5 overflow-x-hidden">
+      <div className="relative flex flex-col items-center w-2/5 gap-10">
         <Clock />
         <TaskForm />
       </div>
-      <div className="w-full bg-black bg-opacity-40 backdrop-blur-xl p-5 mt-10 mx-5 rounded-xl animate-fade-left">
+      <div className="w-full bg-black bg-opacity-40 backdrop-blur-xl p-5 rounded-xl animate-fade-left">
         <DataTable columns={columns} data={tasks} />
       </div>
     </main>
