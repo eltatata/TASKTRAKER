a7d7fd0a74f15345aec14fe81b7312f3f0150748
eltatataa <davidtowo75@gmail.com>
2024-05-18 19:48:07 -0500

    home page content changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link"
 
+import { ArrowRight } from 'lucide-react';
+
 import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
 import { Vortex } from "@/components/ui/vortex";
 
@@ -7,7 +9,7 @@ const words = `A fullstack platform to manage your tasks and be more efficient`;
 
 export default function Home() {
   return (
-    <main className="h-[90vh] overflow-hidden">
+    <main className="h-[91vh] overflow-hidden">
       <Vortex
         backgroundColor="black"
         className="flex items-center flex-col justify-center w-full h-full"
@@ -15,12 +17,15 @@ export default function Home() {
         <h2 className="text-7xl font-bold animate-fade animate-duration-[2000ms] animate-delay-[600ms]">TASKTRAKER</h2>
         <TextGenerateEffect words={words} />
         <div className="pt-5 animate-fade animate-delay-[2000ms]">
-          <Link className="font-semibold text-blue-500 text-lg hover:underline" href="/sign-up">Create an account </Link>
-          or <Link className="font-semibold text-blue-500 text-lg hover:underline" href="/sign-in">Sing in</Link> to get started.
+          <Link
+            href="/tasks"
+            className="flex items-center gap-1 p-3 rounded-md font-semibold text-[#8b5cf6] text-lg hover:underline bg-[#171717] hover:bg-[#262626] transition duration-200"
+          >
+            Get Started
+            <ArrowRight className="w-6 h-6" />
+          </Link>
         </div>
       </Vortex>
     </main>
   )
 }
-
-
