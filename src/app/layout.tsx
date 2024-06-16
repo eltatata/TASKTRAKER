ef050e3162d38728006f068e0898581486b9fc2f
eltatataa <davidtowo75@gmail.com>
2024-06-16 00:48:56 -0500

    error extra attributes from the server fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
         baseTheme: dark
       }}
     >
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning >
         <body className={inter.className}>
           <ThemeProvider
             attribute="class"
