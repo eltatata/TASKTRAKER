3af83dbdeca366e9f64151ec4e54ea3b46b92940
eltatataa <davidtowo75@gmail.com>
2024-06-28 01:07:45 -0500

    changed spinner

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,9 @@
-import { Spinner } from '@/components/ui/spinner';
+import { Loader } from 'lucide-react';
 
 export default function Loading() {
   return (
     <div className='flex justify-center gap-2 pt-10 '>
-      <Spinner size="small" />
+      <Loader size="1.5em" className='animate-spin' />
       <p className='text-xl font-bold'>
         Loading...
       </p>
