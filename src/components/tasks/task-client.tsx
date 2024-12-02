568acb421ec624ecfdaa701bd06d7e9b2b72d8b9
eltatataa <davidtowo75@gmail.com>
2024-12-01 23:39:50 -0500

    changes in the detail view of a task

--- src/components/tasks/task-client.tsx
+++ src/components/tasks/task-client.tsx
@@ -10,6 +10,8 @@ import {
 } from 'lucide-react';
 import Markdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+import { Separator } from '../ui/separator';
+import { priorities, statuses } from "./data"
 import EditTask from './edit-task';
 import DeleteAlert from "./delete-task";
 
@@ -25,58 +27,84 @@ export default function TaskClient({ task }: { task: Task }) {
   if (!isClient) return null
 
   const details = [
-    { key: 'Created At', value: task.createdAt.toLocaleTimeString() },
-    { key: 'Start Date', value: task.startDate?.toLocaleTimeString() },
-    { key: 'End Date', value: task.endDate?.toLocaleTimeString() },
-    { key: 'Priority', value: task.priority },
-    { key: 'Status', value: task.status },
+    {
+      key: 'Status', content:
+        statuses.map((status) => (
+          status.value === task.status && (
+            <div key={status.value} className="flex items-center">
+              {status.icon && (
+                <status.icon className={`mr-1 h-4 w-4 ${status.color}`} />
+              )}
+              <p>{status.label}</p>
+            </div>
+          )
+        ))
+    },
+    {
+      key: 'Priority', content:
+        priorities.map((priority) => (
+          priority.value === task.priority && (
+            <div key={priority.value} className="flex items-center">
+              {priority.icon && (
+                <priority.icon className={`mr-1 h-4 w-4`} />
+              )}
+              <p>{priority.label}</p>
+            </div>
+          )
+        ))
+    },
+    { key: 'Created At', content: <p>{task.createdAt?.toLocaleTimeString()}</p> },
+    { key: 'Start Date', content: task.startDate ? <p>{task.startDate?.toLocaleTimeString()}</p> : <p className='text-muted'>not defined</p> },
+    { key: 'End Date', content: task.endDate ? <p>{task.endDate?.toLocaleTimeString()}</p> : <p className='text-muted'>not defined</p> },
   ]
 
   return (
     <>
       <div className='mx-auto lg:w-3/4 space-y-4'>
-        <Link
-          href="/tasks"
-          className='inline-flex gap-2 border px-3 py-2 rounded-md font-semibold bg-white text-black'
-        >
-          <ArrowLeft className="h-6 w-6" />
-          Tasks
-        </Link>
+        <div className='flex justify-between items-center'>
+          <Link
+            href="/tasks"
+            className='inline-flex gap-2 border px-3 py-2 rounded-md font-semibold bg-white text-black'
+          >
+            <ArrowLeft className="h-6 w-6" />
+            Tasks
+          </Link>
+          <div className='flex gap-4'>
+            <span
+              className='hover:opacity-50 transition-opacity duration-200 ease-in-out cursor-pointer'
+              onClick={() => setOpenEdit(true)}
+            >
+              <Edit className="h-6 w-6" />
+            </span>
+            <span
+              className='hover:opacity-50 text-red-500 transition-opacity duration-200 ease-in-out cursor-pointer'
+              onClick={() => setOpenDelete(true)}
+            >
+              <Trash className="h-6 w-6" />
+            </span>
+          </div>
+        </div>
         <div className="flex gap-8 rounded-xl p-10 animate-fade-left bg-black bg-opacity-5 backdrop-blur-sm">
           <div className="flex-1 space-y-6">
-            <div className="flex items-center gap-10 border-b pb-8 pr-8">
-              <h1 className="text-5xl font-bold">{task.title}</h1>
-              <span
-                className='hover:opacity-50 transition-opacity duration-200 ease-in-out cursor-pointer'
-                onClick={() => setOpenEdit(true)}
-              >
-                <Edit className="h-6 w-6" />
-              </span>
-              <span
-                className='hover:opacity-50 transition-opacity duration-200 ease-in-out cursor-pointer'
-                onClick={() => setOpenDelete(true)}
-              >
-                <Trash className="h-6 w-6" />
-              </span>
+            <h1 className="text-5xl font-bold">{task.title}</h1>
+            <Separator />
+            <div className='space-y-4'>
+              {details.map((detail) => (
+                <div key={detail.key} className="flex items-center gap-2">
+                  <p className="font-semibold">{detail.key}:</p>
+                  {detail.content}
+                </div>
+              ))}
             </div>
+            <Separator 
+              className='border-2 border-gray-500 rounded-full'
+            />
             <Markdown
-              className="w-full prose dark:prose-invert"
+              className="prose max-w-none dark:prose-invert"
               remarkPlugins={[remarkGfm]}
             >
               {task?.description || "Task without description"}
             </Markdown>
-          </div>
-          <div className="space-y-6">
-            <h2 className="text-4xl font-semibold">Details:</h2>
-            <ul className="space-y-5">
-              {details.map((detail) => (
-                <li key={detail.key}>
-                  <p>
-                    <b>{detail.key}:</b> {detail.value}
-                  </p>
-                </li>
-              ))}
-            </ul>
           </div>
         </div>
       </div>
